ac1c7f8689298ce713022cdf5c5024fa7d480fe1
Ayyaz Ahmed <AAhmed@eimagine.com>
2025-05-25 01:33:28 -0400

    Config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/callmeyaz.github.io/react-form-runner-app/",
+  base: "/react-form-runner-app/",
 })
